479d7efc0628b78ef9e4f237f685ff98885b7114
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2021-05-16 00:01:56 -0700

    Initial Commit

---
+++ Shared/TV_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TV_TrackerApp.swift
+//  Shared
+//
+//  Created by Tim Roesner on 5/16/21.
+//
+
+import SwiftUI
+
+@main
+struct TV_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
