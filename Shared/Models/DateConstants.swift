e500f230ff56cf7dacf7ae7abff48bfc76f0834a
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2021-06-26 17:59:01 -0700

    Add Margins and DateConstants

---
+++ Shared/Models/DateConstants.swift
@@ -0,0 +1,15 @@
+//
+//  DateConstants.swift
+//  TV Tracker
+//
+//  Created by Tim Roesner on 6/20/21.
+//
+
+import Foundation
+
+extension TimeInterval {
+    static let minute: TimeInterval = 60
+    static let hour: TimeInterval = 60 * .minute
+    static let day: TimeInterval = 24 * .hour
+    static let week: TimeInterval = 7 * .day
+}
---
+++ Shared/Models/Margins.swift
@@ -0,0 +1,16 @@
+//
+//  Margins.swift
+//  TV Tracker (iOS)
+//
+//  Created by Tim Roesner on 6/22/21.
+//
+
+import UIKit
+
+extension CGFloat {
+    static let tightMargin: CGFloat = 4
+    static let standardMargin: CGFloat = 8
+    static let mediumMargin: CGFloat = 12
+    static let wideMargin: CGFloat = 16
+    static let extraWideMargin: CGFloat = 20
+}
